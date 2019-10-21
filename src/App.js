05ac1e6ea0aa7adc7c3d136e65012ddbdf8020de
mister-ard97 <rezardiansyah1997@gmail.com>
2019-10-21 12:38:22 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import { Modal } from 'reactstrap';
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+
 import {URL_API} from './supports/URL';
 import numeral from 'numeral'
 
 class App extends Component {
 
   state = {
+    idAddRecipe: 1,
+    jumlahBuah: 0,
+    materialChoosed: null,
     editSelected: null,
     loading: false, 
     loadingData: false,
@@ -29,29 +34,105 @@ class App extends Component {
     })
   }
 
+  renderAddRecipe = () => {
+    return (
+      <Modal isOpen={this.state.openModal} toggle={this.toggle} size='xl'>
+        <ModalHeader toggle={this.toggle}>Nutrition Calculation</ModalHeader>
+        <ModalBody>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12'>
+              <input type='text' className='form-control' placeholder='Masukkan Nama Recipe' ref={(NamaRecipe) => this.NamaRecipe = NamaRecipe}/>
+            </div>
+            <div className='col-12'>
+              <Button className='btn btn-secondary' onClick={() => this.setState({jumlahBuah: this.state.jumlahBuah + 1})}>
+                Tambah Bahan
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <table class="table mt-5">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Material Name</th>
+                        <th scope="col">Weight (gram)</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.renderJumlahBuah()}
+
+                    </tbody>
+                  </table>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.saveRecipe}>Save</Button>{' '}
+          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    )
+  }
+
+  saveRecipe = () => {
+    alert(this.NamaRecipe.value)
+    alert(this.refs.NamaBahan0.refs.value)
+  }
+
+  renderJumlahBuah = () => {
+    if(this.state.jumlahBuah !== 0) {
+      var Arrjsx = []
+
+      for(let x = 0; x < this.state.jumlahBuah; x++) {
+          Arrjsx.push(
+            <tr>
+            <td>
+              {x + 1}
+            </td>
+            <td>
+             <Input type='select' innerRef={`namaBahan${x}`}>
+                <option value=''>Pilih bahan</option>
+                {
+                  this.state.data.map((val, index) => {
+                    return <option key={index} value={val.id}>{val.material}</option>
+                  })
+                }
+             </Input>
+            </td>   
+            <td>
+                <input type='number' className='form-control' placeholder='Masukkan berat buah' />
+            </td>
+
+        </tr>
+          )        
+      }
+
+      return Arrjsx;
+    }
+
+    return (
+      <tr col='4'>
+          <td>Belum ada data buah</td>
+      </tr>
+    )
+  }
+
   renderDataNutrisi = () => {
     if(this.state.data.length !== 0) {
       let jsx = this.state.data.map((val, index) => {
         if(this.state.editSelected === val.id) {
          return (
            <tr key={index}>
-             <td>{val.material}</td>
+             <td>
+               {val.id}
+             </td>
+             <td>
+               <input type='select'>
+                 <option></option>
+               </input>
+             </td>
              <td>
                <input ref={(beratBuah) => this.beratBuah = beratBuah} type='number' placeholder='Masukkan berat buah' />
-             </td>
-             <td>{val.calorie}</td>
-             <td>{parseFloat(val.protein * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.totalFat * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.saturated * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.mufa * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.pufa * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.carbohydrat * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.fiber * 100).toFixed(2)}</td>
-             <td>Rp. {val.price}</td>
-             <td>
-               <a href={`${val.source_url}`} target='_blank' rel='noopener noreferrer'>
-                 Detail Buah
-               </a>  
              </td>
              <td>
                <input type='button' className='btn btn-success' value='Save' onClick={() => this.EditDataBuah()}/> 
@@ -65,21 +146,13 @@ class App extends Component {
         
         return (
          <tr key={index}>
-           <td>{val.material} </td>
-           <td>100 gram</td>
-           <td>{val.calorie}</td>
-           <td>{parseFloat(val.protein * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.totalFat * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.saturated * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.mufa * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.pufa * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.carbohydrat * 100).toFixed(2)}</td>
-           <td>{parseFloat(val.fiber * 100).toFixed(2)}</td>
-           <td>Rp. {numeral(val.price * 100).format(0, 0)}</td>
-           <td>
-               <a href={`${val.source_url}`} target='_blank' rel='noopener noreferrer'>
-                 Detail Buah
-               </a>  
+            <td>
+               {val.id}
+             </td>
+             <td>
+               <input type='select'>
+                 <option></option>
+               </input>
              </td>
            <td>
              <input type='button' className='btn btn-success' value='Ubah Berat Buah' onClick={() => this.setState({editSelected: val.id})}/> 
@@ -116,7 +189,7 @@ class App extends Component {
               </div>
             </div>
           </div>
-
+            {this.renderAddRecipe(this.state.openModal)}
             <div className='container'>
               <div className='col-12'>
                 <input type='button' className='btn btn-secondary form-control mb-3' value='Add New Recipe' onClick={() => this.setState({ openModal: true})}/>
@@ -125,29 +198,7 @@ class App extends Component {
               </div>
             </div>
 
-              <table class="table mt-5">
-                    <thead class="thead-dark">
-                      <tr>
-                        <th scope="col">Material Name</th>
-                        <th scope="col">Weight (gram)</th>
-                        <th scope="col">Calorie (kcal)</th>
-                        <th scope="col">Protein</th>
-                        <th scope="col">Total Fat</th>
-                        <th scope="col">Saturated</th>
-                        <th scope="col">MUFA</th>
-                        <th scope="col">PUFA</th>
-                        <th scope="col">Carbohydrate</th>
-                        <th scope="col">Fiber</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Source</th>
-                        <th scope="col">Edit</th>
-                        <th scope="col">Delete</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {this.renderDataNutrisi()}
-                    </tbody>
-                  </table>
+              
 
       </div>
     )
