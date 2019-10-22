c24dc866165465c158f231c39092adb3f73e4d12
mister-ard97 <rezardiansyah1997@gmail.com>
2019-10-22 17:24:12 +0700

    yuvens

--- src/supports/URL.js
+++ src/supports/URL.js
@@ -1,3 +1,3 @@
-export const URL_API = 'http://localhost:2002';
+// export const URL_API = 'http://localhost:2002';
 
-// export const URL_API = 'https://kasihnus.purwadhikax.com';
+export const URL_API = 'https://kasihnus.purwadhikax.com';
